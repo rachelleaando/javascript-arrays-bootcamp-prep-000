7ad6dfa56a9ed9a435ba0ba851ee765cf3897356
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-12-10 15:29:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,90 +1,20 @@
-
 var chocolateBars = [
-  "snickers", "hundred grand", "kitkat", "skittles"
+  'sneakers', 'hundred grand', 'kitkat', 'skittles'
   ]
   
-
-//***************************************************************
+  function addElementToBeginningOfArray(array, element) {
+    return [element, ...array]
+  }
   
-function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
-}
-
-
-//***************************************************************
-
-function descructivelyAddElementToBeginningOfArray (array, element) {
-  array.unshift(element);
-  return array;
-}
-function destructivelyAddElementToBeginningOfArray (array, element) {
+  function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
-  return array
-}
-
-
-//***************************************************************
-function addElementToEndOfArray (array, element) {
-  array.push(element)
-  return array
-}
-
-
-//***************************************************************
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
   return array;
-}
-
-
-//***************************************************************
-function addElementToEndOfArray(array, element)  {
-  return [...array, element];
-}
-
-
-//***************************************************************
-function accessElementInArray(array, index) {
-  return array[index];
-}
-
-
-//***************************************************************
-function destructivelyRemoveElementFromBeginningOfArray (array) {
-  array.shift();
-  return array;
-}
-
-
-//***************************************************************
-function removeElementFromBeginningOfArray (array) {
-  array.slice();
-  return array;
-}
-
-
-//***************************************************************
-function removeElementFromBeginningOfArray (array) {
-  return array.slice(1);
-}
-
-
-//***************************************************************
-function destructivelyRemoveElementFromEndOfArray (array) {
-  array.pop();
-  return array;
-}
-
-
-//***************************************************************
-function removeElementFromEndOfArray (array) {
-  array.pop();
-  return array;
-}
-
-
-//***************************************************************
-function removeElementFromEndOfArray (array) {
-  return array.slice(0, array.length - 1);
-}
-
+  }
+  
+  // Is it possible to put only "array" or "element" to the parameter???
+  function addElementToEndOfArray(array, element) {
+    return [array, ...element]
+  }
+  
+  function destructivelyAddElementToEndOfArray()
+  
